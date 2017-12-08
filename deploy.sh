0e875fb531ec4819173bfbd74eabdc3e6914f0ce
Tom CzHen <tom.czhen@gmail.com>
2017-12-08 21:32:28 +0800

    修正拼写错误

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 export HEXO_WWW_PATH=/data/hexo
-docker run --rm -v $(pwd):/blog -v $(pwd)/hexo_config.yml:/hexo/_configy.yml -v $(pwd)/theme_next_config.yml:/hexo/themes/next/_config.yml -v ${HEXO_WWW_PATH}:/wwwroot hexo
+docker run --rm -v $(pwd):/blog -v $(pwd)/hexo_config.yml:/hexo/_config.yml -v $(pwd)/theme_next_config.yml:/hexo/themes/next/_config.yml -v ${HEXO_WWW_PATH}:/wwwroot hexo
