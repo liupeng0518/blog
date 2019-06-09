ad03a8c7582b8439a54f83cabb5d5bbdbb0bdb52
Tom CzHen <tom.czhen@gmail.com>
2019-06-09 17:56:40 +0800

    去掉 hard code

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,7 @@ export CADDY_ROOT=/root/caddy
 export CADDYPATH=${CADDY_ROOT}/assets
 export CADDY_WWW_ROOT=${CADDY_ROOT}/www
 export CADDY_LOG_ROOT=${CADDY_ROOT}/logs
+export CADDY_REPO_ROOT=${CADDY_ROOT}/repo
 mkdir -p ${CADDY_WWW_ROOT}/${CADDY_DOMAIN} ${CADDY_LOG_ROOT}/${CADDY_DOMAIN}
 
 exec "$@"
